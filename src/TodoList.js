c082bf692ce8f0b4257f0a063b047626168a13bd
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-14 23:33:46 +0900

    휴

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { useState } from 'react';
 
-// function upload(props) {
-//   const upload = document.getElementsByClassName('todo_btn');
-//   if()
-// }
-
 function TodoList(props) {
-  const[click,setClick] = useState('');
+  const [inputValue, setValue] = useState('뭐해야 헀더라');
+  const [click, setClick] = useState('');
   return (
-    <button type='button' onClick={props.onClick}>
-      입력
-    </button>
+    <div>
+      <div>
+        <input type='text' id='todo' name='todo' placeholder={inputValue} />
+      </div>
+      <p>{click}</p>
+      <button type='button' onClick={() => setClick(setValue(inputValue))}>
+        입력
+      </button>
+    </div>
   );
 }
 
