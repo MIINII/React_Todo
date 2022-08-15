12e3aec1aad30fab72f1d9d8d498a33d23705814
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-15 21:47:02 +0900

    layout

--- todo/src/layout/Layout.jsx
+++ todo/src/layout/Layout.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './layout.css';
-import Header from '../header/Header';
-import Form from '../form/Form';
+
+// import Form from '../form/Form';
 import TodoList from '../pages/TodoList';
 
 function Layout() {
   return (
-    <div id='layout_wrap'>
-      <Header />
-      <Form />
-      <TodoList />
+    <div>
+      <TodoList/>
+      {/* <Form /> */}
     </div>
   );
 }
