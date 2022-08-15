1692cfca8d2e44ae98d3056961d814e087deef3e
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-15 21:47:22 +0900

    todo_list

--- todo/src/pages/TodoList.jsx
+++ todo/src/pages/TodoList.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './todo_list.css';
 
+import Form from '../form/Form';
 import Todo from '../todo/Todo';
-
 
 function TodoList() {
   return (
     <div id='todolist_wrap'>
-      <Todo />
+      <Todo text="잘되는거 마즈?" done={true} />
+      <Todo text="잘되는거 마즈?" done={true} />
+      <Todo text="잘되는거 마즈?" done={false} />
+      <Todo text="잘되는거 마즈?" done={true} />
+      <Form />
     </div>
   );
 }
