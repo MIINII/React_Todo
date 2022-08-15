e19693531789d0a4cb92cb67589fd4601a8a6a26
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-15 21:46:21 +0900

    form 만들기

--- todo/src/form/Form.jsx
+++ todo/src/form/Form.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import './form.css';
 import { useState } from 'react';
 
-const Form = props => {
-  const [inputValue, setValue] = useState('뭐해야 헀더라');
-  const [click, setClick] = useState('');
+const Form = todo => {
+  const [click, setClick] = useState(false);
+  const onToggle = () => setClick(!click);
   return (
     <div className='allForm'>
-      <input className='todo_input' type='text' name='todo_title' placeholder={inputValue}></input>
-      <input className='todo_input' type='text' name='todo_con' placeholder={inputValue}></input>
+      <input className='todo_input' type='text' placeholder='뭐해야했더라' />
 
-      <button type='button' onClick={() => setClick(setValue(inputValue))}>
+      <button onclick={onToggle} click={click} type='button'>
         입력
       </button>
     </div>
   );
 };
 
-export default Form
+export default Form;
