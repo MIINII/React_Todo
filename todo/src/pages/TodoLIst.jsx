120497427821d537348181f9a1f7fc654b035217
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-17 13:17:19 +0900

    pages

---
+++ todo/src/pages/TodoLIst.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './todo_list.css';
+import Layout from '../layout/Layout';
+
+const TodoList = () => {
+  return (
+    <div className='todo_wrap'>
+      <Layout />
+    </div>
+  );
+};
+
+export default TodoList;
